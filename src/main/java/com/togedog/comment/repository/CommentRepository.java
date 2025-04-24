c3eaf27485f0cf8267ba28dfc11411ca792defa2
chaeneeee <cheche0804@naver.com>
2025-04-25 02:53:19 +0900

    redis 수정

--- src/main/java/com/togedog/comment/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.togedog.comment.repository;
-
-import com.togedog.comment.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
--- src/main/java/com/togedog/reply/repository/ReplyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.togedog.reply.repository;
-
-import com.togedog.reply.entity.Reply;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReplyRepository extends JpaRepository<Reply, Long> {
-}
